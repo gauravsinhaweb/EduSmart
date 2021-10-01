7c1ae1cbb6130f0ffc84ccfd3d9c9a12a8ef7ee2
Mihir Harshe <mihir.harshe12@gmail.com>
2021-10-01 23:18:19 +0530

    added icons of contributor's accounts and placed the names in a single row

--- client/src/pages/Contributors.js
+++ client/src/pages/Contributors.js
@@ -6,30 +6,30 @@ function Contributors() {
       <div className="text-3xl text-white font-extralight ml-40 mt-8">
         Contributors🤝
       </div>{" "}
-      <div className="flex flex-col mx-80 my-40">
+      <div className="flex justify-between mx-80 my-40">
         <div>
           <div className="text-white text-xl">
             <div className="uppercase my-4 font-medium text-secondary">
               Harshal Raikwar
             </div>
-            <div className="flex flex-row ">
+            <div className="flex flex-row justify-center gap-6">
               {" "}
-              <div className="text-sm font-light m-4 underline  ml-0">
+              <div className="text-sm font-light  underline  ml-0">
                 <a
                   href="https://www.linkedin.com/in/harshalraikwal/"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  GitHub
+                  <i class='bx bxl-github icon-size'></i>
                 </a>{" "}
               </div>
-              <div className="text-sm font-light  m-4 underline  ml-0">
+              <div className="text-sm font-light   underline  ml-0">
                 <a
                   href="https://www.linkedin.com/in/harshalraikwal/"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  LinkedIn
+                  <i class='bx bxl-linkedin icon-size' ></i>
                 </a>{" "}
               </div>
             </div>
@@ -40,24 +40,24 @@ function Contributors() {
             <div className="uppercase my-4 font-medium text-secondary">
               Gaurav Sinha
             </div>
-            <div className="flex flex-row ">
+            <div className="flex flex-row justify-center gap-6">
               {" "}
-              <div className="text-sm font-light m-4 underline  ml-0">
+              <div className="text-sm font-light  underline  ml-0">
                 <a
                   href="https://github.com/gauravsinhaweb"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  GitHub
+                  <i class='bx bxl-github icon-size'></i>
                 </a>{" "}
               </div>
-              <div className="text-sm font-light  m-4 underline  ml-0">
+              <div className="text-sm font-light   underline  ml-0">
                 <a
                   href="https://www.linkedin.com/in/gaurav-sinha-032386187/"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  LinkedIn
+                  <i class='bx bxl-linkedin icon-size' ></i>
                 </a>{" "}
               </div>
             </div>
@@ -69,24 +69,24 @@ function Contributors() {
             <div className="uppercase my-4 font-medium text-secondary">
               Ritesh Kumar
             </div>
-            <div className="flex flex-row ">
+            <div className="flex flex-row justify-center gap-6">
               {" "}
-              <div className="text-sm font-light m-4 underline  ml-0">
+              <div className="text-sm font-light  underline  ml-0">
                 <a
                   href="https://www.linkedin.com/in/harshalraikwal/"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  GitHub
+                  <i class='bx bxl-github icon-size'></i>
                 </a>{" "}
               </div>
-              <div className="text-sm font-light  m-4 underline  ml-0">
+              <div className="text-sm font-light   underline  ml-0">
                 <a
                   href="https://www.linkedin.com/in/harshalraikwal/"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  LinkedIn
+                  <i class='bx bxl-linkedin icon-size' ></i>
                 </a>{" "}
               </div>
             </div>
@@ -97,24 +97,24 @@ function Contributors() {
             <div className="uppercase my-4 font-medium text-secondary">
               Riya Gupta
             </div>
-            <div className="flex flex-row ">
+            <div className="flex flex-row justify-center gap-6">
               {" "}
-              <div className="text-sm font-light m-4 underline  ml-0">
+              <div className="text-sm font-light  underline  ml-0">
                 <a
                   href="https://www.linkedin.com/in/harshalraikwal/"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  GitHub
+                  <i class='bx bxl-github icon-size'></i>
                 </a>{" "}
               </div>
-              <div className="text-sm font-light  m-4 underline  ml-0">
+              <div className="text-sm font-light   underline  ml-0">
                 <a
                   href="https://www.linkedin.com/in/harshalraikwal/"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
-                  LinkedIn
+                  <i class='bx bxl-linkedin icon-size' ></i>
                 </a>{" "}
               </div>
             </div>
