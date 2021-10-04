2bb8d9d44b3202aab7c19787ed89fa3b9a1cda40
Ansh Narula <2019210@iiitdmj.ac.in>
2021-10-04 16:20:57 +0530

    Added reusable card component and used it in Classroom and Chatroom pages

--- client/src/pages/Classroom.js
+++ client/src/pages/Classroom.js
@@ -1,52 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from "../components/Card";
 import { ClassRoomIcon } from "../assets/icons";
 export function Classroom() {
-    return (
-        <>
-            <div className="w-full h-screen">
-                <div className="text-3xl text-white font-extralight mt-8 text-center">
-                    Classroom🎓
-                </div>
-                <div className="flex justify-around mt-16">
-                    <Link to="medical">
-                        <div className="h-64 w-80 bg-secondary rounded-2xl cursor-pointer 	active:bg-highlight 	">
-                            <div className="text-white text-2xl flex justify-center h-full items-center font-medium">
-                                Medical
-                            </div>
-                        </div>
-                    </Link>
-
-                    <Link to="aerospacePage">
-                        <div className="h-64 w-80 bg-secondary rounded-2xl cursor-pointer 	active:bg-highlight ">
-                            <div className="text-white text-2xl flex justify-center h-full items-center font-medium">
-                                Aerospace <br /> Engineering
-                            </div>
-                        </div>
-                    </Link>
-
-                    <Link to="planets">
-                        <div className="h-64 w-80 bg-secondary rounded-2xl cursor-pointer 	active:bg-highlight ">
-                            <div className="text-white text-2xl flex justify-center h-full items-center font-medium">
-                                Our Planets
-                            </div>
-                        </div>
-                    </Link>
-                </div>
-                <div className="flex justify-around  mt-16 opacity-90">
-                    <div>
-                        <div className="h-64 w-80 bg-secondary rounded-2xl cursor-pointer 	active:bg-highlight ">
-                            <div className="text-white text-2xl flex justify-center h-full items-center font-medium">
-                                Coming Soon...
-                            </div>
-                        </div>
-                    </div>
-                    <div className="opacity-60">
-                        <ClassRoomIcon />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="w-full h-screen">
+        <div className="text-3xl text-white font-extralight mt-8 text-center">
+          Classroom🎓
+        </div>
+        <div className="flex justify-around mt-16">
+          <Link to="medical">
+            <Card title="Medical" />
+          </Link>
+          <Link to="aerospacePage">
+            <Card title="Aerospace Engineering" />
+          </Link>
+          <Link to="planets">
+            <Card title="Planets" />
+          </Link>
+        </div>
+        <div className="flex justify-around  mt-16 opacity-90">
+          <Card title="Coming Soon ..." />
+          <div className="opacity-60">
+            <ClassRoomIcon />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
