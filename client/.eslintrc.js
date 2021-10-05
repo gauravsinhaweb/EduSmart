fe8d388f51f5f57d1cb1424898bbc007e74256e6
girordo <tsgiroldo@gmail.com>
2021-10-05 17:37:55 -0300

    :sparkles: | adding files: eslintrc, prettierrc and editorconfig

---
+++ client/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": "off"
+  }
+};
