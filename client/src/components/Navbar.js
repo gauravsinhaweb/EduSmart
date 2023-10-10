46f057c145ffef441a7ed442d2aedbd5985c33d8
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2023-10-10 22:42:59 +0530

    change the ui for the sidebar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillChatDotsFill } from "react-icons/bs";
@@ -10,51 +10,61 @@ import { useHistory } from "react-router";
 
 const Navbar = ({ onHover, setOnHover = () => {} }) => {
   const history = useHistory();
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
       <div
-        className={`rounded-r-2xl h-screen sticky top-0 z-50" bg-tertiary transition-all group w-1/6 max-w-max min-w-max ${
+        className={`ml-5 rounded-lg h-screen sticky top-0 left-0 z-50 bg-tertiary transition-all group w-16 max-w-max min-w-max ${
           !onHover ? "sm:w-20" : "sm:w-64"
-        }`}
-      >
+        } ${scrolling ? "blur-effect" : ""}`}>
         <div
           onClick={() => setOnHover(!onHover)}
           className={`text-white font-light text-2xl p-1 w-12 ml-4  py-12 cursor-pointer   ${
             onHover ? "hidden" : null
-          }`}
-        >
+          }`}>
           <HiMenuAlt1 />
         </div>
         <div className={`${onHover ? "hidden" : null}`}>
           <div
             onClick={() => history.push("home")}
-            className="mt-6  cursor-pointer opacity-80 hover:opacity-100  text-white font-light text-2xl p-1 w-8 ml-4 border-b border-white border-opacity-50 py-4"
-          >
+            className="mt-6  cursor-pointer opacity-80 hover:opacity-100  text-white font-light text-2xl p-1 w-8 ml-4 border-b border-white border-opacity-50 py-4">
             <AiFillHome />
           </div>
           <div
             onClick={() => history.push("classroom")}
-            className="mt-6  cursor-pointer opacity-80 hover:opacity-100  text-white font-light text-2xl p-1 w-8 ml-4 border-b border-white border-opacity-50 py-4"
-          >
+            className="mt-6  cursor-pointer opacity-80 hover:opacity-100  text-white font-light text-2xl p-1 w-8 ml-4 border-b border-white border-opacity-50 py-4">
             <SiGoogleclassroom />
           </div>
           <div
             onClick={() => history.push("chat")}
-            className="mt-6  cursor-pointer opacity-80 hover:opacity-100  text-white font-light text-2xl p-1 w-8 ml-4 border-b border-white border-opacity-50 py-4"
-          >
+            className="mt-6  cursor-pointer opacity-80 hover:opacity-100  text-white font-light text-2xl p-1 w-8 ml-4 border-b border-white border-opacity-50 py-4">
             <BsFillChatDotsFill />
           </div>
           <div
             onClick={() => history.push("contributors")}
-            className="mt-6  cursor-pointer opacity-80 hover:opacity-100  text-white font-light text-2xl p-1 w-8 ml-4 border-b border-white border-opacity-50 py-4"
-          >
+            className="mt-6  cursor-pointer opacity-80 hover:opacity-100  text-white font-light text-2xl p-1 w-8 ml-4 border-b border-white border-opacity-50 py-4">
             <RiTeamFill />
           </div>
           <div
             onClick={() => history.push("faq")}
-            className="mt-6  cursor-pointer opacity-80 hover:opacity-100  text-white font-light text-2xl p-1 w-8 ml-4 border-b border-white border-opacity-50 py-4"
-          >
+            className="mt-6  cursor-pointer opacity-80 hover:opacity-100  text-white font-light text-2xl p-1 w-8 ml-4 border-b border-white border-opacity-50 py-4">
             <GiMagnifyingGlass />
           </div>
         </div>
@@ -66,14 +76,12 @@ const Navbar = ({ onHover, setOnHover = () => {} }) => {
             onClick={() => setOnHover(!onHover)}
             className={`ml-48 py-12 text-white font-light text-2xl p-1 w-12 ml-2 cursor-pointer ${
               !onHover ? "hidden" : null
-            }`}
-          >
+            }`}>
             <HiMenuAlt3 />
           </div>
           <div
             onClick={() => history.push("home")}
-            className="flex  m-8 cursor-pointer opacity-90 hover:opacity-100 "
-          >
+            className="flex  m-8 cursor-pointer opacity-90 hover:opacity-100 ">
             <span className="text-white px-2  text-2xl">
               <AiFillHome />
             </span>
@@ -84,8 +92,7 @@ const Navbar = ({ onHover, setOnHover = () => {} }) => {
           </div>
           <div
             onClick={() => history.push("classroom")}
-            className="flex m-8 cursor-pointer opacity-90 hover:opacity-100"
-          >
+            className="flex m-8 cursor-pointer opacity-90 hover:opacity-100">
             <span className="text-white px-2  text-2xl flex h-12  align-center">
               <SiGoogleclassroom />
             </span>
@@ -96,8 +103,7 @@ const Navbar = ({ onHover, setOnHover = () => {} }) => {
           </div>
           <div
             onClick={() => history.push("chat")}
-            className="flex m-8 cursor-pointer opacity-90 hover:opacity-100"
-          >
+            className="flex m-8 cursor-pointer opacity-90 hover:opacity-100">
             <span className="text-white px-2  text-2xl flex h-12  align-center">
               <BsFillChatDotsFill />
             </span>
@@ -108,8 +114,7 @@ const Navbar = ({ onHover, setOnHover = () => {} }) => {
           </div>
           <div
             onClick={() => history.push("contributors")}
-            className="flex m-8 cursor-pointer opacity-90 hover:opacity-100"
-          >
+            className="flex m-8 cursor-pointer opacity-90 hover:opacity-100">
             <span className="text-white px-2  text-2xl flex h-12  align-center">
               <RiTeamFill />
             </span>
@@ -121,8 +126,7 @@ const Navbar = ({ onHover, setOnHover = () => {} }) => {
 
           <div
             onClick={() => history.push("faq")}
-            className="flex m-8 cursor-pointer opacity-90 hover:opacity-100"
-          >
+            className="flex m-8 cursor-pointer opacity-90 hover:opacity-100">
             <span className="text-white px-2  text-2xl flex h-12  align-center">
               <GiMagnifyingGlass />
             </span>
