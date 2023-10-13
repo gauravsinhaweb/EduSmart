b003cdfb6fd7f6e173601dafab4c9bbbcba057c4
GAURAV SINHA <sinhagaurav.me@gmail.com>
2023-10-13 16:19:47 +0530

    changes

--- client/src/pages/Faq.js
+++ client/src/pages/Faq.js
@@ -1,34 +1,7 @@
 import React, { useState } from "react";
 import { BsQuestionCircleFill } from "react-icons/bs";
 import faqImg from "../assets/faq1.png";
-
-const faqData = [
-  {
-    question: "What is EduSmart?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi iusto quam doloremque perferendis aliquam numquam architecto nemo magnam ducimus molestiae.",
-  },
-  {
-    question: "What it can bring to you?",
-    answer:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia nobis at nisi. Nemo doloribus a sequi impedit tempora velit omnis autem, minima rem.",
-  },
-  {
-    question: "Have more question or want to contribute?",
-    answer:
-      "Awesome! we would be more than happy to hear from you. Reach out at xyz@gmail.com. See you soon, Cheere!",
-  },
-  {
-    question: "Can you work as a team?",
-    answer:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt recusandae assumenda expedita labore eius repudiandae libero quod iure, adipisci temporibus.",
-  },
-  {
-    question: "How many team members can you invite?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil fugit ea sequi voluptas minima.",
-  },
-];
+import { faqData } from "../utils/constants";
 
 export const Faq = () => {
   const [activeIndex, setActiveIndex] = useState([]);
