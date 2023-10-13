16aeae0898f51791355c1778d30f242b8f101349
GAURAV SINHA <sinhagaurav.me@gmail.com>
2023-10-13 16:12:57 +0530

    changes

--- client/src/pages/Faq.js
+++ client/src/pages/Faq.js
@@ -6,33 +6,28 @@ const faqData = [
   {
     question: "What is EduSmart?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi iusto quam doloremque perferendis aliquam numquam architecto nemo magnam ducimus molestiae."
-  },
-  {
-    question: "Why EduSmart?",
-    answer:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit facilis est quasi modi. Explicabo debitis esse perspiciatis at labore quia."
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi iusto quam doloremque perferendis aliquam numquam architecto nemo magnam ducimus molestiae.",
   },
   {
     question: "What it can bring to you?",
     answer:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia nobis at nisi. Nemo doloribus a sequi impedit tempora velit omnis autem, minima rem."
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia nobis at nisi. Nemo doloribus a sequi impedit tempora velit omnis autem, minima rem.",
   },
   {
     question: "Have more question or want to contribute?",
     answer:
-      "Awesome! we would be more than happy to hear from you. Reach out at xyz@gmail.com. See you soon, Cheere!"
+      "Awesome! we would be more than happy to hear from you. Reach out at xyz@gmail.com. See you soon, Cheere!",
   },
   {
     question: "Can you work as a team?",
     answer:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt recusandae assumenda expedita labore eius repudiandae libero quod iure, adipisci temporibus."
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt recusandae assumenda expedita labore eius repudiandae libero quod iure, adipisci temporibus.",
   },
   {
     question: "How many team members can you invite?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil fugit ea sequi voluptas minima."
-  }
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil fugit ea sequi voluptas minima.",
+  },
 ];
 
 export const Faq = () => {
@@ -41,9 +36,9 @@ export const Faq = () => {
   function handleActiveIndex(index) {
     const THEREINDEX = activeIndex.includes(index);
     if (THEREINDEX) {
-      const filteredArray = activeIndex.filter(e => e !== index);
+      const filteredArray = activeIndex.filter((e) => e !== index);
       setActiveIndex(filteredArray);
-    } else setActiveIndex(prevIndex => [...prevIndex, index]);
+    } else setActiveIndex((prevIndex) => [...prevIndex, index]);
   }
 
   return (
@@ -69,14 +64,16 @@ export const Faq = () => {
                 return (
                   <details
                     className="pb-5 border-b md:w-4/5 cursor-pointer"
-                    key={index}>
+                    key={index}
+                  >
                     <summary
                       onClick={() => handleActiveIndex(index)}
                       className={`card font-medium ${
                         activeIndex.includes(index)
                           ? "text-secondary font-semibold"
                           : "hover:text-red "
-                      }`}>
+                      }`}
+                    >
                       {faq.question}
                     </summary>
                     <p className="text-gray-400 md:text-md  font-sem font-light	opacity-70 ">
