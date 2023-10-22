9afef7c0c2139592906d4d9e78b547184b73ca7a
Kevin Zhu <u7338066@anu.edu.au>
2023-10-23 07:39:08 +1100

    Fixed title text alignment for different classrooms

--- client/src/components/Medical.js
+++ client/src/components/Medical.js
@@ -3,7 +3,7 @@ import { PlantCell, AnimalCell, Skeleton } from "./3DModel";
 const Medical = () => {
   return (
     <div>
-      <div className="text-3xl text-white font-extralight ml-48 mt-8">
+      <div className="text-3xl text-white font-extralight text-center mt-8">
         Medical
       </div>
       <PlantCell />
